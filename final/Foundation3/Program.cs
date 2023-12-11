7eb3b9e34c27302e3b4a0b9ecee4cbab27d9664e
LosAllen <lincolnjallen@gmail.com>
2023-12-11 13:18:48 -0600

    Finished final programs

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,201 @@
 using System;
 
+class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+
+    public Address(string street, string city, string state, string zipCode)
+    {
+        Street = street;
+        City = city;
+        State = state;
+        ZipCode = zipCode;
+    }
+
+    public override string ToString()
+    {
+        return $"{Street}, {City}, {State} {ZipCode}";
+    }
+}
+
+class Event
+{
+    private string title;
+    private string description;
+    private DateTime date;
+    private TimeSpan time;
+    private Address address;
+
+    public Event(string title, string description, DateTime date, TimeSpan time, Address address)
+    {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+        this.address = address;
+    }
+
+    public string Title { get { return title; } }
+    public string Description { get { return description; } }
+    public DateTime Date { get { return date; } }
+    public TimeSpan Time { get { return time; } }
+    public Address Address { get { return address; } }
+
+    public string GenerateStandardMessage()
+    {
+        return $"Event: {title}\nDescription: {description}\nDate: {date.ToShortDateString()}\nTime: {time}\nAddress: {address}";
+    }
+
+    public virtual string GenerateFullMessage()
+    {
+        return GenerateStandardMessage();
+    }
+
+    public string GenerateShortDescription()
+    {
+        return $"Event Type: Generic\nTitle: {title}\nDate: {date.ToShortDateString()}";
+    }
+}
+
+class Lecture : Event
+{
+    private string speaker;
+    private int capacity;
+
+    public Lecture(string title, string description, DateTime date, TimeSpan time, Address address, string speaker, int capacity)
+        : base(title, description, date, time, address)
+    {
+        this.speaker = speaker;
+        this.capacity = capacity;
+    }
+
+    public override string GenerateFullMessage()
+    {
+        return $"{base.GenerateFullMessage()}\nType: Lecture\nSpeaker: {speaker}\nCapacity: {capacity}";
+    }
+}
+
+class Reception : Event
+{
+    private string rsvpEmail;
+
+    public Reception(string title, string description, DateTime date, TimeSpan time, Address address, string rsvpEmail)
+        : base(title, description, date, time, address)
+    {
+        this.rsvpEmail = rsvpEmail;
+    }
+
+    public override string GenerateFullMessage()
+    {
+        return $"{base.GenerateFullMessage()}\nType: Reception\nRSVP Email: {rsvpEmail}";
+    }
+}
+
+class OutdoorGathering : Event
+{
+    private string weatherStatement;
+
+    public OutdoorGathering(string title, string description, DateTime date, TimeSpan time, Address address, string weatherStatement)
+        : base(title, description, date, time, address)
+    {
+        this.weatherStatement = weatherStatement;
+    }
+
+    public override string GenerateFullMessage()
+    {
+        return $"{base.GenerateFullMessage()}\nType: Outdoor Gathering\nWeather Statement: {weatherStatement}";
+    }
+}
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        // Get user inputs for each event
+        Console.WriteLine("Enter details for the Generic Event:");
+        Event genericEvent = GetUserInputForEvent();
+
+        Console.WriteLine("\nEnter details for the Tech Talk (Lecture):");
+        Lecture lectureEvent = GetUserInputForLecture();
+
+        Console.WriteLine("\nEnter details for the Networking Mixer (Reception):");
+        Reception receptionEvent = GetUserInputForReception();
+
+        Console.WriteLine("\nEnter details for the Summer Picnic (Outdoor Gathering):");
+        OutdoorGathering outdoorEvent = GetUserInputForOutdoorGathering();
+
+        // Display generated messages for each event
+        Console.WriteLine("\nGenerated Messages:");
+        Console.WriteLine("\nGeneric Event:\n" + genericEvent.GenerateStandardMessage() + "\n");
+        Console.WriteLine("Tech Talk:\n" + lectureEvent.GenerateFullMessage() + "\n");
+        Console.WriteLine("Networking Mixer:\n" + receptionEvent.GenerateShortDescription() + "\n");
+        Console.WriteLine("Summer Picnic:\n" + outdoorEvent.GenerateFullMessage() + "\n");
+    }
+
+    static Event GetUserInputForEvent()
+    {
+        Console.Write("Title: ");
+        string title = Console.ReadLine();
+
+        Console.Write("Description: ");
+        string description = Console.ReadLine();
+
+        Console.Write("Date (MM/DD/YYYY): ");
+        t
+
+        Console.Write("Time (HH:mm): ");
+        TimeSpan time = TimeSpan.Parse(Console.ReadLine());
+
+        Console.Write("Street Address: ");
+        string street = Console.ReadLine();
+
+        Console.Write("City: ");
+        string city = Console.ReadLine();
+
+        Console.Write("State: ");
+        string state = Console.ReadLine();
+
+        Console.Write("Zip Code: ");
+        string zipCode = Console.ReadLine();
+
+        Address address = new Address(street, city, state, zipCode);
+
+        return new Event(title, description, date, time, address);
+    }
+
+    static Lecture GetUserInputForLecture()
+    {
+        Event baseEvent = GetUserInputForEvent();
+
+        Console.Write("Speaker: ");
+        string speaker = Console.ReadLine();
+
+        Console.Write("Capacity: ");
+        int capacity = int.Parse(Console.ReadLine());
+
+        return new Lecture(baseEvent.Title, baseEvent.Description, baseEvent.Date, baseEvent.Time, baseEvent.Address, speaker, capacity);
+    }
+
+    static Reception GetUserInputForReception()
+    {
+        Event baseEvent = GetUserInputForEvent();
+
+        Console.Write("RSVP Email: ");
+        string rsvpEmail = Console.ReadLine();
+
+        return new Reception(baseEvent.Title, baseEvent.Description, baseEvent.Date, baseEvent.Time, baseEvent.Address, rsvpEmail);
+    }
+
+    static OutdoorGathering GetUserInputForOutdoorGathering()
+    {
+        Event baseEvent = GetUserInputForEvent();
+
+        Console.Write("Weather Statement: ");
+        string weatherStatement = Console.ReadLine();
+
+        return new OutdoorGathering(baseEvent.Title, baseEvent.Description, baseEvent.Date, baseEvent.Time, baseEvent.Address, weatherStatement);
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,141 @@
 using System;
 
+class Activity
+{
+    private string _date;
+    private int _length;
+
+    public Activity(string date, int length)
+    {
+        _date = date;
+        _length = length;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0; // To be overridden in derived classes
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0; // To be overridden in derived classes
+    }
+
+    public virtual double GetPace()
+    {
+        return 0; // To be overridden in derived classes
+    }
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} {GetType().Name} ({_length} min)";
+    }
+}
+
+class Running : Activity
+{
+    private double _distance;
+
+    public Running(string date, int length, double distance) : base(date, length)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return _distance / base.GetDistance() * 60;
+    }
+
+    public override double GetPace()
+    {
+        return base.GetDistance() / _distance;
+    }
+
+    public override string GetSummary()
+    {
+        string distanceUnit = "miles";
+        string speedUnit = "mph";
+        string paceUnit = "min per mile";
+        return $"{base.GetSummary()} - Distance {GetDistance()} {distanceUnit}, Speed {GetSpeed():F1} {speedUnit}, Pace: {GetPace():F1} {paceUnit}";
+    }
+}
+
+class Cycling : Activity
+{
+    private double _speed;
+
+    public Cycling(string date, int length, double speed) : base(date, length)
+    {
+        _speed = speed;
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override string GetSummary()
+    {
+        string speedUnit = "kph";
+        return $"{base.GetSummary()} - Speed: {GetSpeed():F1} {speedUnit}";
+    }
+}
+
+class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(string date, int length, int laps) : base(date, length)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return _laps * 50 / 1000.0;
+    }
+
+    public override string GetSummary()
+    {
+        string distanceUnit = "km";
+        return $"{base.GetSummary()} - Distance {GetDistance():F1} {distanceUnit}";
+    }
+}
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        var activities = new Activity[3];
+
+        // Get user inputs
+        Console.Write("Enter date (DD MMM YYYY): ");
+        string date = Console.ReadLine();
+
+        Console.Write("Enter length of activity in minutes: ");
+        int length = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter running distance in miles: ");
+        double distance = double.Parse(Console.ReadLine());
+        activities[0] = new Running(date, length, distance);
+
+        Console.Write("Enter cycling speed in kph: ");
+        double speed = double.Parse(Console.ReadLine());
+        activities[1] = new Cycling(date, length, speed);
+
+        Console.Write("Enter swimming laps: ");
+        int laps = int.Parse(Console.ReadLine());
+        activities[2] = new Swimming(date, length, laps);
+
+        // Display summaries
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
